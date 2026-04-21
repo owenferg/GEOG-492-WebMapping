e61a765770370d100d51abb51bf82ea6f07b4a4d
owenferg <owenf@uoregon.edu>
2026-04-20 20:50:20 -0700

    clean up code

--- Assignment3/assignment3.js
+++ Assignment3/assignment3.js
@@ -1,5 +1,7 @@
+// mapbox access token
 mapboxgl.accessToken = "pk.eyJ1Ijoib3dlbmZlcmciLCJhIjoiY21uaHp6a3Z5MDg5NjJwb2RrdTVpbDhxbCJ9.i_URRCdviydaQxvfgjhVfw";
 
+// map setup
 var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/owenferg/cmnz6sujk003u01st26p0407m',
@@ -7,19 +9,22 @@ var map = new mapboxgl.Map({
     zoom: 8.5,
 });
 
+// navigation control
 const nav = new mapboxgl.NavigationControl({})
 map.addControl(nav,'top-left');
 
+// panel state
 var state = { panelOpen: true };
 
+// toggle description panel
 function panelSelect(e){
-if(state.panelOpen){
-    document.getElementById('descriptionPanel').style.height = '26px';
-    document.getElementById('glyph').className = "chevron glyphicon glyphicon-chevron-up";
-    state.panelOpen = false;
-} else {
-    document.getElementById('descriptionPanel').style.height = '250px';
-    document.getElementById('glyph').className = "chevron glyphicon glyphicon-chevron-down";
-    state.panelOpen = true;
-}
+    if(state.panelOpen){
+        document.getElementById('descriptionPanel').style.height = '26px';
+        document.getElementById('glyph').className = "chevron glyphicon glyphicon-chevron-up";
+        state.panelOpen = false;
+    } else {
+        document.getElementById('descriptionPanel').style.height = '250px';
+        document.getElementById('glyph').className = "chevron glyphicon glyphicon-chevron-down";
+        state.panelOpen = true;
+    }
 }
